397688a6dcd5bd8a9a29218f208315c2758e19df
zimplex <zhqhku@gmail.com>
2016-03-21 13:40:31 +0800

    2 more example

--- example/misc1.py
+++ example/misc1.py
@@ -16,8 +16,28 @@ def schedule1():
     s.save()
 
 
+def schedule2():
+    U_x = 60
+    U_y = 30
+    s = misc.job_scheduling.scheduler('draw/example/schedule2.png', U_x, U_y)
+    s.add_job(0,5,'green')
+    s.add_job(5,7,'blue')
+    s.drawing()
+    s.save()
 
+
+def schedule3():
+    U_x = 60
+    U_y = 30
+    s = misc.job_scheduling.scheduler('draw/example/schedule3.png', U_x, U_y)
+    s.add_job(0,1,'green')
+    s.add_job(1,3,'blue')
+    s.add_job(3,7, 'green')
+    s.drawing()
+    s.save()
 
 
 if __name__ == '__main__':
     schedule1()
+    schedule2()
+    schedule3()
